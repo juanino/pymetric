775961107e5476aaa21fe4bc918b322aea540664
Jerry Uanino <juanino@gmail.com>
2017-09-09 21:49:43 -0400

    added modules and main program

---
+++ getmodules.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+pip install BeautifulSoup
+pip install ClientForm
+pip install PyRRD
+pip install SQLAlchemy
+pip install argparse
+pip install boto
+pip install boto3
+pip install botocore
+pip install distribute
+pip install docutils
+pip install fernet
+pip install futures
+pip install glacier
+pip install iotop
+pip install iso8601
+pip install jmespath
+pip install mechanize
+pip install mod-python
+pip install ordereddict
+pip install ping
+pip install prettytable
+pip install py-rrdtool
+pip install pyaes
+pip install pyping
+pip install python-apt
+pip install python-dateutil
+pip install python-eeml
+pip install pytz
+pip install reportbug
+pip install s3transfer
+pip install simplejson
+pip install six
+pip install wsgiref
+pip install youtube-dl
---
+++ pymetric.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+import time
+import ping
+import time
+import socket
+
+# get the graphite server from config file
+import pymetric_config as cfg
+
+def collect_metric(name, value, timestamp):
+    sock = socket.socket()
+    sock.connect( (cfg.graphite_server, 2003) )
+    sock.send("%s %d %d\n" % (name, value, timestamp))
+    sock.close()
+
+def now():
+    return int(time.time())
+
+#collect_metric("pymetric.ping.switch", 49, now())
+
+hosts = ["8.8.8.8","www.ibm.com","www.yahoo.com"]
+
+while True:
+    for host in hosts:
+        try:
+            hname = host.replace('.','_')
+            metric_name = "pymetric.ping." + hname
+            print "working on " + metric_name
+            result =  ping.quiet_ping(host, count=5)
+            [loss, maxrtt, artt] = result
+            print loss, maxrtt, artt
+            if loss == 100:
+                print "loss is : " + str(loss)
+                print "skipping report on artt"
+                #raise Exception('bad data')
+            else:
+                collect_metric("pymetric.ping."+hname, artt, now())
+        except socket.error, e:
+            print "Ping Error:", e
+    print "sleeping..."
+    time.sleep(10)
